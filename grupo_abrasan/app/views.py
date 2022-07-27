fa386c1150b5895ade88c6cd69f32009b76896e9
mons-castrosix <monse.castrosixtos@gmail.com>
2022-07-27 18:31:43 -0500

    .

--- grupo_abrasan/app/views.py
+++ grupo_abrasan/app/views.py
@@ -1,8 +1,7 @@
 
 from email import message
 from email.headerregistry import Group
-from math import prod
-from msilib.schema import File
+
 from typing import Type
 import xml.etree.ElementTree as ET
 from turtle import pen, title
