c4184ab6213782519612410b0ec50da65af5b050
mons-castrosix <monse.castrosixtos@gmail.com>
2022-08-02 13:56:34 -0500

    Update models.py

--- grupo_abrasan/app/models.py
+++ grupo_abrasan/app/models.py
@@ -86,5 +86,5 @@ fs = FileSystemStorage(location='app/upload')
 class Archivos(models.Model):
     solicitud = models.TextField(max_length=3)
     fecha = models.DateField(auto_now_add=True)
-    ruta = models.FileField(upload_to='xml/')
+    ruta = models.FileField(upload_to='xml/',max_length=254)
     
